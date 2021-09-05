70483defc2167e3c7a74ee310932165117f3358a
geoff-nunan <39071789+geoff-nunan@users.noreply.github.com>
2021-09-06 09:09:39 +1000

    initial commit (#1)
    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # react-dnd-task
-React dnd assessment task
+This repo contains the source code for a drag-and-drop task board.
+
+![image](Screenshot.png)
+
+## Assessment Tasks:
+### 1. Extend the project so that new tasks can be added
+### 2. Extend the project so that tasks can be edited and saved
+### 3. Extend the project so that columns can be dragged into a different order.
+
+## How to submit your work.
+Create a new branch for your work.
+Commit you work into this new branch
+Submit a Pull Request for you changes.
---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import { createGlobalStyle } from 'styled-components'
+
+// Import main Board component
+import { Board } from './components/board'
+
+// Use createGlobalStyle to change the background of 'body' element
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #4bcffa;
+  }
+`
+
+// Create component for the page
+const Page = () => (<>
+  <Board />
+  <GlobalStyle />
+</>)
+
+// Render the page into DOM
+ReactDOM.render(<Page />, document.getElementById('root'))
+
